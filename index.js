58c0578b31ea7b59cd6c1bac05fdfbd9e791f7ee
Ryohei Ueda <garaemon@gmail.com>
2017-04-01 02:55:44 +0900

    Show dialog to choose file if no file is specified

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
 const electron = require('electron');
 const electron_reload = require('electron-reload');
-const util = require('util');
-const ejs = require('ejs');
-const fs = require('fs');
 const log = require('winston');
 const minimist = require('minimist');
 const openAboutWindow = require('about-window').default;
-const path = require('path');
 
 const argv = minimist(process.argv.slice(2));
 
@@ -30,16 +30,58 @@ log.addColors({
 log.remove(log.transports.Console);
 log.add(log.transports.Console, {level: 'debug', colorize:true, timestamp: true});
 
-const target_file = process.argv[2];
-const is_debug_mode = 'debug' in argv && argv['debug'];
-
-if (!fs.existsSync(target_file)) {
-  log.error(`${target_file} does not exists.`);
-  process.exit(1);
-}
-
 // prevent window being garbage collected
 let mainWindow;
+
+function openWithFile(target_file, is_debug_mode) {
+  if (!fs.existsSync(target_file)) {
+    log.error(`${target_file} does not exists.`);
+    process.exit(1);
+  }
+  setupMenu();
+  if (is_debug_mode) {
+    mainWindow.webContents.openDevTools();
+  }
+  mainWindow.webContents.on('did-finish-load', function() {
+    log.info(`send notify-file event to open ${target_file}`);
+    mainWindow.send('notify-file', target_file);
+  });
+  // start watching file
+  fs.watch(target_file, function() {
+    log.info(`detect change on ${target_file}`);
+    mainWindow.send('notify-file', target_file);
+  });
+  electron.ipcMain.on('copy-to-clipboard', function(event, arg) {
+    if (arg) {
+      log.info(`write to clipboard: ${arg}`);
+      // CAVEAT: writeText does not work in tmux environment on OS X.
+      electron.clipboard.writeText(arg);
+    }
+  });
+};
+
+function main() {
+  // TODO: remove options from process.argv
+  const is_debug_mode = 'debug' in argv && argv['debug'];
+  if (!mainWindow) {
+    mainWindow = createMainWindow();
+  }
+
+  if (process.argv.length < 3) {
+    // Open dialog file if no file is specified
+    electron.dialog.showOpenDialog(mainWindow, function(file_paths) {
+      if (!file_paths || file_paths.length != 1) {
+        log.error(`Please choose one file`);
+        process.exit(1);
+      } else {
+        openWithFile(file_paths[0], is_debug_mode);
+      }
+    });
+  } else {
+    const target_file = process.argv[2];
+    openWithFile(target_file, is_debug_mode);
+  }
+};
 
 function onClosed() {
   // dereference the window
@@ -99,27 +141,5 @@ app.on('activate', () => {
 });
 
 app.on('ready', () => {
-  if (!mainWindow) {
-    mainWindow = createMainWindow();
-  }
-  setupMenu();
-  if (is_debug_mode) {
-    mainWindow.webContents.openDevTools();
-  }
-  mainWindow.webContents.on('did-finish-load', function() {
-    log.info(`send notify-file event to open ${target_file}`);
-    mainWindow.send('notify-file', target_file);
-  });
-  // start watching file
-  fs.watch(target_file, function() {
-    log.info(`detect change on ${target_file}`);
-    mainWindow.send('notify-file', target_file);
-  });
-  electron.ipcMain.on('copy-to-clipboard', function(event, arg) {
-    if (arg) {
-      log.info(`write to clipboard: ${arg}`);
-      // CAVEAT: writeText does not work in tmux environment on OS X.
-      electron.clipboard.writeText(arg);
-    }
-  });
+  main();
 });
