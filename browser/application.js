8b69d4feb6d12be69ee08f09a4eaf4323286c8d2
Ryohei Ueda <garaemon@gmail.com>
2017-04-15 02:07:51 +0900

    Add Application class

---
+++ browser/application.js
@@ -0,0 +1,158 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const electron = require('electron');
+const electronReload = require('electron-reload');
+const electronDebug = require('electron-debug');
+const openAboutWindow = require('about-window').default;
+
+const log = require('winston');
+
+const menu = require('./menu');
+
+class Application {
+  constructor(commandLineArgs) {
+    this.args = commandLineArgs;
+    // adds debug features like hotkeys for triggering dev tools and reload
+    electronDebug();
+
+    // reload if any change is detected.
+    electronReload(path.join(__dirname, '..'));
+
+    this._setupLogger();
+
+    this._registerAppCallbacks();
+  }
+
+  /**
+   * setup log info and coloring.
+   */
+  _setupLogger() {
+    log.addColors({
+      debug: 'green',
+      info:  'cyan',
+      silly: 'magenta',
+      warn:  'yellow',
+      error: 'red'
+    });
+
+    log.remove(log.transports.Console);
+    log.add(log.transports.Console, {level: 'debug', colorize: true, timestamp: true});
+  }
+
+  /**
+   * register callback methods to electron.app.
+   */
+  _registerAppCallbacks() {
+    electron.app.on('activate', () => { this._onActivate(); });
+    electron.app.on('window-all-closed', () => { this._onWindowAllClosed(); });
+    electron.app.on('ready', () => { this._onReady(); });
+  }
+
+  _createMainWindow() {
+    const win = new electron.BrowserWindow({
+      width: 1200,
+      height: 800
+    });
+    const htmlFile = `file://${__dirname}/../index.html`;
+    win.loadURL(htmlFile);
+    this.mainWindow = win;
+    win.on('closed', () => { this.onClosed(); });
+    return win;
+  }
+
+  _showAboutApplication() {
+    openAboutWindow({
+      icon_path: path.join(__dirname, '../resources/logo.png'),
+      copyright: 'Copyright (c) 2017 garaemon'
+    });
+  }
+
+  _run() {
+    if (!this.mainWindow) {
+      this._createMainWindow();
+    }
+    const isDebugMode = this.args.debug;
+    if (this.args._.length == 0) {
+      electron.dialog.showOpenDialog(this.mainWindow, (filePaths) => {
+        if (!filePaths || filePaths.length != 1) {
+          log.error(`Please choose one file`);
+          process.exit(1);
+        } else {
+          this._openWithFile(filePaths[0], isDebugMode);
+        }
+      });
+    } else {
+      const targetFile = this.args._[0];
+      this._openWithFile(targetFile, isDebugMode);
+    }
+  }
+
+  _openWithFile(target_file, is_debug_mode) {
+    if (!fs.existsSync(target_file)) {
+      log.error(`${target_file} does not exists.`);
+      process.exit(1);
+    }
+    electron.Menu.setApplicationMenu(menu);
+    if (is_debug_mode) {
+      this.mainWindow.webContents.openDevTools();
+    }
+    this.mainWindow.webContents.on('did-finish-load', () => {
+      log.info(`send notify-file event to open ${target_file}`);
+      this.mainWindow.send('notify-file', target_file);
+    });
+    // start watching file
+    fs.watch(target_file, function() {
+      log.info(`detect change on ${target_file}`);
+      this.mainWindow.send('notify-file', target_file);
+    });
+    electron.ipcMain.on('copy-to-clipboard', function(event, arg) {
+      if (arg) {
+        log.info(`write to clipboard: ${arg}`);
+        // CAVEAT: writeText does not work in tmux environment on OS X.
+        electron.clipboard.writeText(arg);
+      }
+    });
+
+  }
+
+  // Callback methods
+
+  /**
+   * Callback method when 'closed' event of electron.app is fired.
+   */
+  _onClosed() {
+    this.mainWindow = null;
+  }
+
+  /**
+   * Callback method when 'activate' event of electron.app is fired.
+   */
+  _onActivate() {
+    log.info('_onActivate');
+    if (this.mainWindow) {
+      this.mainWindow = this._createMainWindow();
+    }
+  }
+
+  /**
+   * callback method when 'window-all-closed' event of electron.app is fired.
+   */
+  _onWindowAllClosed() {
+    if (process.platform !== 'darwin') {
+      electron.app.quit();
+    }
+  }
+
+  /**
+   * callback method when 'ready' event of electron.app is fired.
+   */
+  _onReady() {
+    log.info('_onReady');
+    this._run();
+  }
+}
+
+module.exports = Application;
