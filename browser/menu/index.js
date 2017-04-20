c3085b01a74758154efecfeed0ce6b02a0e40952
Ryohei Ueda <garaemon@gmail.com>
2017-04-21 00:37:56 +0900

    Add toggle-toc to menu

--- browser/menu/index.js
+++ browser/menu/index.js
@@ -2,9 +2,10 @@ const electron = require('electron');
 
 const FileMenu = require('./FileMenu.js');
 const PrimaryMenu = require('./PrimaryMenu.js');
+const ViewMenu = require('./ViewMenu.js');
 
 module.exports = function(app) {
-  const menus = [new PrimaryMenu(app), new FileMenu(app)];
+  const menus = [new PrimaryMenu(app), new FileMenu(app), new ViewMenu(app)];
   return electron.Menu.buildFromTemplate(menus.map(function(menu) {
     return menu.buildTemplate();
   }));
