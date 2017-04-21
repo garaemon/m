701130092cefa62e0361107e64711b168685988e
Ryohei Ueda <garaemon@gmail.com>
2017-04-22 00:26:15 +0900

    Add build task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@
 // Recomment to run with gulper command.
 // gulper is a wrapper command of gulp to restart gulp when gulpfile.s is updated.
 // gulper command can be installed by 'npm install -g gulper'.
+const fs = require('fs');
 
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 
 const jsSources = ['index.js', 'gulpfile.js',
                    './browser/**/*.js', './renderer/**/*.js'];
+const iconPath = 'dist/icon.icns';
 
 gulp.task('lint', () => {
   return gulp.src(jsSources)
@@ -47,5 +49,43 @@ gulp.task('lint', () => {
 gulp.task('watch', ['lint'], () => {
   gulp.watch(jsSources, ['lint']);
 });
+
+gulp.task('icon', () => {
+  // remove iconPath first if it exists
+  if (fs.existsSync(iconPath)) {
+    fs.unlinkSync(iconPath);
+  }
+  return gulp.src('resources/logo.png')
+    .pipe($.exec(`nicns --in resources/logo.png --out ${iconPath}`))
+    .pipe($.exec.reporter({
+      err: true,
+      stderr: true,
+      stdout: true
+    }));
+});
+
+function buildElectronPackagerCommands(platforms) {
+  return platforms.map((platform) => {
+    return `electron-packager . m --platform=${platform} --arch=x64 --icon=${iconPath}` +
+      `--overwrite --out build`;
+  });
+}
+const electronBuildCommands = buildElectronPackagerCommands(['darwin']);
+
+console.log(electronBuildCommands);
+electronBuildCommands.forEach((command) => {
+  gulp.task(command, ['icon'], () => {
+    return gulp.src('')
+      .pipe($.exec(command, {
+        pipeStdout: true
+      }))
+      .pipe($.exec.reporter({
+        err: true,
+        stderr: true,
+        stdout: true
+      }));
+  });
+});
+gulp.task('build', electronBuildCommands);
 
 gulp.task('default', ['lint']);
