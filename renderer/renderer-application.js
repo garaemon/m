1450079af2944116834a2b16e84cda0336883b26
Ryohei Ueda <garaemon@gmail.com>
2017-04-23 16:51:42 +0900

    Add RendererApplication class as frontend application class

---
+++ renderer/renderer-application.js
@@ -0,0 +1,72 @@
+const electron = require('electron');
+
+const frontendPlugins = require('./frontend_plugins.js');
+
+const MarkdownRenderer = electron.remote.require('./browser/markdown_renderer.js');
+
+// Application class for render process
+class RenderApplication {
+  constructor() {
+    this.registerIpcCallbacks();
+    this.runPreprocessPlugins();
+  }
+
+  runPreprocessPlugins() {
+    for (const plugin of frontendPlugins.plugins) {
+      plugin.runPreprocess();
+    }
+  }
+
+  runPostprocessPlugins() {
+    for (const plugin of frontendPlugins.plugins) {
+      plugin.runPostprocess();
+    }
+  }
+
+  // register callback methods for ipc
+  registerIpcCallbacks() {
+    const ipcRenderer = electron.ipcRenderer;
+    ipcRenderer.on('notify-file', (event, file) => this.notifyFileCallback(file));
+    ipcRenderer.on('notify-error', (event, error) => this.notifyErrorCallback(error));
+    ipcRenderer.on('toggle-toc', () => this.toggleTocCallback());
+  }
+
+  getMainView() {
+    return document.getElementById('main-view');
+  }
+
+  renderFile(file) {
+    const renderer = new MarkdownRenderer();
+    const mainView = this.getMainView();
+    renderer.render(file, (err, renderedResult) => {
+      if (err) {
+        mainView.showError(err);
+      } else {
+        mainView.updateView(renderedResult);
+        this.runPostprocessPlugins();
+      }
+    });
+  }
+
+  toggleTocCallback() {
+    const mainView = this.getMainView();
+    mainView.toggleTOC();
+  }
+
+  notifyErrorCallback(error) {
+    if (error) {
+      const mainView = this.getMainView();
+      mainView.showError(error);
+    }
+  }
+
+  notifyFileCallback(file) {
+    this.renderFile(file);
+  }
+
+  run() {
+    // Do nothing
+  }
+}
+
+module.exports = RenderApplication;
