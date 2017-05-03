cdf661ff0d161867ec10c08b2f31c259ff8aa545
Ryohei Ueda <garaemon@gmail.com>
2017-05-03 17:34:03 +0900

    Support --detach in bin/m.js instead of index.js

--- bin/m.js
+++ bin/m.js
@@ -6,15 +6,27 @@
 // It just returns a path
 const electronPath = require('electron-prebuilt');
 const childProcess = require('child_process');
+const minimist = require('minimist');
 
 // Adjust the command line arguments: remove the "node <code.js>" part
 const args = process.argv.slice(2);
+const parsedArgs = minimist(process.argv.slice(2));
 
 // ... and insert the root path of our application (it's the parent directory)
 // as the first argument
 args.unshift(__dirname + '/../');
 
-// Run electron
-childProcess.spawn(electronPath, args, {
-  stdio: 'inherit',
-});
+if (parsedArgs.detach) {
+  // remove --detach from process.argv
+  const detachIndex = args.indexOf('--detach');
+  args.splice(detachIndex, 1);
+  childProcess.spawn(electronPath, args, {
+    detached: true,
+    stdio: 'ignore',
+  }).unref();
+} else {
+  // Run electron
+  childProcess.spawn(electronPath, args, {
+    stdio: 'inherit',
+  });
+}
--- index.js
+++ index.js
@@ -2,24 +2,10 @@
 
 'use strict';
 
-const childProcess = require('child_process');
-
 const minimist = require('minimist');
 
-// local libraries
 const Application = require('./browser/application.js');
 
 const argv = minimist(process.argv.slice(2));
-if (argv.detach) {
-  // remove --detach
-  const detachIndex = process.argv.indexOf('--detach');
-  process.argv.splice(detachIndex, 1);
-  childProcess.spawn(process.argv[0], process.argv.slice(1), {
-    detached: true,
-    stdio: 'ignore',
-  }).unref();
-  process.exit(0);
-} else {
-  const app = new Application(argv);
-  app.run();
-}
+const app = new Application(argv);
+app.run();
