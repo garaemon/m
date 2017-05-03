55300960c7a2754e32fe5072896667a12a2b4326
Ryohei Ueda <garaemon@gmail.com>
2017-05-03 17:28:47 +0900

    Add bin/m.js for entry point from command line

---
+++ bin/m.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+// Entry point for command line
+// http://blog.soulserv.net/building-a-package-featuring-electron-as-a-stand-alone-application/
+
+// It just returns a path
+const electronPath = require('electron-prebuilt');
+const childProcess = require('child_process');
+
+// Adjust the command line arguments: remove the "node <code.js>" part
+const args = process.argv.slice(2);
+
+// ... and insert the root path of our application (it's the parent directory)
+// as the first argument
+args.unshift(__dirname + '/../');
+
+// Run electron
+childProcess.spawn(electronPath, args, {
+  stdio: 'inherit',
+});
