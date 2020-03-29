f4a568f8112747f040ff0044ef348a9354b422e0
Ryohei Ueda <garaemon@gmail.com>
2020-03-29 14:18:13 +0900

    Complete emoji candidates
    * Use lodash to debounce changes event.

--- src/renderer/Editor.tsx
+++ src/renderer/Editor.tsx
@@ -5,7 +5,9 @@ import React, { Component } from 'react';
    In order to avoid this issue, use react-codemirror2 package.
  */
 import { UnControlled as CodeMirror } from 'react-codemirror2';
-import * as codemirror from 'codemirror';
+import codemirror from 'codemirror';
+import { debounce } from 'lodash';
+
 const ipcRenderer = window.require('electron').ipcRenderer;
 
 import 'codemirror/lib/codemirror.css';
@@ -17,6 +19,8 @@ import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/markdown/markdown';
 import 'codemirror/mode/gfm/gfm';
+import 'codemirror/addon/hint/show-hint';
+import 'codemirror/addon/hint/show-hint.css';
 
 import 'hypermd/core';
 import 'hypermd/mode/hypermd';
@@ -38,6 +42,7 @@ import 'hypermd/addon/mode-loader';
 import 'hypermd/addon/table-align';
 
 import { IAppConfig } from '../IAppConfig';
+import { emojiList } from './EmojiList';
 
 interface EditorProps {
 }
@@ -49,8 +54,11 @@ interface EditorStates {
 }
 
 export default class Editor extends Component<EditorProps, EditorStates> {
+    private editor: codemirror.Editor | null = null;
+
     constructor(props: EditorProps) {
         super(props);
+        this.editor = null;
         this.state = {
             content: '', shown_content: '',
             config: {
@@ -81,11 +89,74 @@ export default class Editor extends Component<EditorProps, EditorStates> {
         ipcRenderer.send('render-process-ready');
     }
 
-    onBeforeChange(_editor: codemirror.Editor, _data: codemirror.EditorChange, value: string) {
+    onBeforeChange(editor: codemirror.Editor, _data: codemirror.EditorChange, value: string) {
         this.setState({ 'shown_content': value });
         if (!document.title.endsWith('*')) {
             document.title = document.title + '*';
         }
+    }
+
+    onChange(editor: codemirror.Editor) {
+        // not called?
+    }
+
+    onEditorDidMount(editor: codemirror.Editor) {
+        this.editor = editor;
+        /* Use debounce to throttle change event.*/
+        this.editor.on('change', debounce((editor: codemirror.Editor) => {
+            codemirror.showHint(editor, undefined, {
+                completeSingle: false, hint: () => { return this.emojiComplete(editor); },
+            });
+        }, 500));
+    }
+
+    emojiComplete(editor: codemirror.Editor): codemirror.Hints {
+        const cur = editor.getCursor();
+        const token = editor.getTokenAt(cur);
+        const start = token.start;
+        const end = cur.ch;
+        const line = cur.line;
+        const emptyResult = {
+            list: [],
+            from: codemirror.Pos(0, 0),
+            to: codemirror.Pos(0, 0),
+        };;
+
+        // ch is the position of word.
+        // ch starts with current cursor position and move it left until find ':'.
+        // If current line is 'abc :foo bar', The tokens are 'abc', ' ', ':', 'foo', ' ', 'bar'.
+        // Be careful that ':' is not included in a token with trailing characters.
+        let previousWord = token.string;
+        for (let ch = cur.ch;
+            ch > -1 /* Till the beggining of the line*/;
+            --ch /* move cursor to left */) {
+            const currentToken = editor.getTokenAt({ ch, line }).string;
+            // if currentToken is a white space, immediately stops completion because it
+            // means looping over multiple words.
+            if (/\s/.test(currentToken)) {
+                return emptyResult;
+            }
+
+            if (currentToken == ':') {
+                const filteredList = emojiList.filter((item) => {
+                    return item.startsWith(previousWord);
+                });
+                if (filteredList.length > 0) {
+                    return {
+                        list: filteredList.map((candidate) => { return candidate + ':'; }),
+                        from: codemirror.Pos(cur.line, start),
+                        to: codemirror.Pos(cur.line, end)
+                    };
+                }
+            }
+            // Memorize current currentToken for next loop because emoji-like string, such as :foo,
+            // will be devided into two tokens; one is trailing word and the other is ':'.
+            // If the next loop find ':' as currentToken, the completion will be based on
+            // previousWord.
+            previousWord = currentToken;
+        }
+
+        return emptyResult;
     }
 
     render() {
@@ -114,6 +185,8 @@ export default class Editor extends Component<EditorProps, EditorStates> {
         return (<CodeMirror
             value={this.state.content}
             onBeforeChange={this.onBeforeChange.bind(this)}
+            editorDidMount={this.onEditorDidMount.bind(this)}
+            onChange={this.onChange.bind(this)}
             className="code-mirror-editor"
             options={options} />);
     }
