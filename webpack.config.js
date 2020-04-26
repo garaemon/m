8edf39eb16a0f6e6310e039e9081d7b32a1ca93a
Ryohei Ueda <garaemon@gmail.com>
2020-04-26 18:53:41 +0900

    Apply prettier

--- webpack.config.js
+++ webpack.config.js
@@ -8,26 +8,24 @@ const main = {
   entry: path.join(__dirname, 'src', 'index'),
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   node: {
     __dirname: false,
-    __filename: false
+    __filename: false,
   },
   module: {
-    rules: [{
-      test: /.ts?$/,
-      include: [
-        path.resolve(__dirname, 'src'),
-      ],
-      exclude: [
-        path.resolve(__dirname, 'node_modules'),
-      ],
-      loader: 'ts-loader',
-    }]
+    rules: [
+      {
+        test: /.ts?$/,
+        include: [path.resolve(__dirname, 'src')],
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        loader: 'ts-loader',
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.ts']
+    extensions: ['.js', '.ts'],
   },
 };
 
@@ -37,46 +35,45 @@ const render = {
   entry: path.join(__dirname, 'src', 'renderer', 'index'),
   output: {
     path: path.resolve(__dirname, 'dist', 'scripts'),
-    filename: 'index.js'
+    filename: 'index.js',
   },
   resolve: {
-    extensions: ['.json', '.js', '.jsx', '.css', '.ts', '.tsx', '.scss']
+    extensions: ['.json', '.js', '.jsx', '.css', '.ts', '.tsx', '.scss'],
   },
   module: {
-    rules: [{
-      test: /\.(tsx|ts)$/,
-      use: [
-        'ts-loader'
-      ],
-      include: [
-        path.resolve(__dirname, 'src'),
-        path.resolve(__dirname, 'node_modules'),
-      ],
-    }, {
-      test: /\.css$/,
-      exclude: '/node_modules/',
-      use: [
-        'style-loader',
-        {
-          'loader': 'css-loader',
-          options: {
-            url: false
-          }
-        }
-      ]
-    }, {
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            url: false,
+    rules: [
+      {
+        test: /\.(tsx|ts)$/,
+        use: ['ts-loader'],
+        include: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'node_modules')],
+      },
+      {
+        test: /\.css$/,
+        exclude: '/node_modules/',
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            },
           },
-        },
-        'sass-loader',
-      ],
-    }]
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
   },
   plugins: [
     new CopyWebpackPlugin([
@@ -91,9 +88,7 @@ const render = {
         to: 'html',
       },
     ]),
-  ]
+  ],
 };
 
-module.exports = [
-  main, render,
-];
+module.exports = [main, render];
